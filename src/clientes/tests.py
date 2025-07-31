35b4529e7f90f02aecf8132942b071eeb83db0ca
Lipinski1201 <gustavolipinski12@gmail.com>
2025-07-30 21:04:17 -0300

    feat: adicionar API REST de clientes (serializers, viewset, rotas e testes)

--- src/clientes/tests.py
+++ src/clientes/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import Cliente
 
-# Create your tests here.
+class ClienteAPITests(APITestCase):
+    def setUp(self):
+        self.url = reverse("cliente-list")
+        Cliente.objects.create(nome="Teste", email="t@x.com")
+
+    def test_list_clients(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
